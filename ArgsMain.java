e596d9fbbc2a25f63215bfea5f7bbf9b7cb70e37
Baochuan Lu <lubaochuan@gmail.com>
2023-04-03 23:36:35 -0500

    add String arguments

--- ArgsMain.java
+++ ArgsMain.java
@@ -1,14 +1,24 @@
+import java.text.ParseException;
+
 public class ArgsMain {
   public static void main(String[] args) {
-    Args arg = new Args("l,p,d", args);
-    boolean valid = arg.isValid();
-    boolean logging = arg.getBoolean('l');
-    if(valid){
-      System.out.println("The argument format is valid");
-    }else{
-      System.out.println("The argument format is invalid");
+    try{
+      Args arg = new Args("l,d*", args);
+      boolean valid = arg.isValid();
+      boolean logging = arg.getBoolean('l');
+      String directory = arg.getString('d');
+      if(valid){
+        System.out.println("The argument format is valid");
+      }else{
+        System.out.println("The argument format is invalid");
+      }
+
+      System.out.println("logging is "+logging);
+      System.out.println("directory is "+directory);
+    } catch (ParseException e) {
+      System.out.printf("Argument error: %s\n", e.getMessage());
     }
 
-    System.out.println("logging is "+logging);
+
   }
 }
--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-Simple tests for the "Boolean only" version of Args:
+Schema:
+* `char` - Boolean arg.
+* `char*` - String arg.
+
+Example schema: `l,d*`
+Coresponding command line: `-l -d tmp`
+
+Simple tests for the "Boolean and String" version of Args:
 ```
 % javac ArgsMain.java
-% java ArgsMain -l
+% java ArgsMain -l -d tmp
 The argument format is valid
 logging is true
-% java ArgsMain -h
-The argument format is invalid
-logging is false
+directory is tmp
 ```

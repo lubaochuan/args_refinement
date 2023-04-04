6da001cd6a51d3fe240bf683c7d955ef63e5d03f
Baochuan Lu <lubaochuan@gmail.com>
2023-04-03 23:53:00 -0500

    append ArgumentMarshaler to Args without breaking anything

--- Args.java
+++ Args.java
@@ -194,4 +194,23 @@ public class Args {
   public boolean isValid() {
     return valid;
   }
+
+  private class ArgumentMarshaler {
+    private boolean booleanValue = false;
+
+    public void setBoolean(boolean value) {
+      booleanValue = value;
+    }
+    public boolean getBoolean() {return booleanValue;}
+  }
+
+  private class BooleanArgumentMarshaler extends ArgumentMarshaler {
+  }
+
+  private class StringArgumentMarshaler extends ArgumentMarshaler {
+  }
+  private class IntegerArgumentMarshaler extends ArgumentMarshaler {
+  }
 }
+
+
